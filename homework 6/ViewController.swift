719623afc0a64a02300e87ee5ce833f35e03d59c
Frankie Bonura <f.bonura@setonhill.edu>
2020-04-06 13:41:21 -0400

    Initial Commit

---
+++ homework 6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework 6
+//
+//  Created by Frank Bonura on 4/6/20.
+//  Copyright © 2020 Frank Bonura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
